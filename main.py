7236e38effb2cbaae3abfe4c8dfbb224cb7839d8
赵如意 <3438605753@qq.com>
2025-09-13 17:10:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# 这里编写你的代码
+num=input("请输入一个五位数：")
+if len(num)!=5 or not num.isdigit():
+    print("输入错误，请输入一个五位数。")
+else:
+    reversed_num=num[::-1]
+    if num==reversed_num:
+        print(f"{num}是回文数。")
+    else:
+        print(f"{num}不是回文数。")
+
