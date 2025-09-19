1a08e84d47d5fbbcc7158b83aebaa595bbf3ab30
赵如意 <3438605753@qq.com>
2025-09-19 10:33:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-num=input("请输入一个五位数: ")
+num=input('请输入一个五位数: ')
 if len(num)!=5 or not num.isdigit():
-    print("输入错误，请确保输入的是一个五位数!")
+    print('输入错误: 请输入5位数字')
+else:
     if num==num[::-1]:
-        print(f"{num}是一个回文数")
+        print('是回文数')
     else:
-        print(f"{num}不是一个回文数")
+        print('不是回文数')
